48fc4f9b8ea0b47f77db409699c24a52e4c0f5c9
adrigon1503 <adrigon1503@gmail.com>
2025-05-23 19:42:29 +0200

    correcion variables

--- backend/app/database.py
+++ backend/app/database.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_USER = os.getenv("POSTGRES_PASSWORD")
+DB_USER = os.getenv("POSTGRES_USER")
 DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("POSTGRES_DB")
