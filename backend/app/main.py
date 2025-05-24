ef6b2fe2a50cde486a6bdc3a243c1c17928ceac7
adrigon1503 <adrigon1503@gmail.com>
2025-05-24 14:42:36 +0200

    pruebas 6

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,8 +13,9 @@ from . import models, schemas, crud
 from .database import engine, SessionLocal
 from backend.app.logger import get_logger
 from backend.auth.routers import router as auth_router
+from fastapi import FastAPI
 
-
+app = FastAPI()
 app.include_router(auth_router, prefix="/auth")
 
 
